Fixed Log loss function and incorrct derivative assignment.

neural_network.py
@@ -11,7 +11,7 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.datasets import load_breast_cancer
 import matplotlib.pyplot as plt
-
+np.random.seed(23)
 #################################
 # Neural Network class
 #################################
@@ -35,17 +35,18 @@ class NN:
 
     def output_function(self, output='sigmoid'):
         self.output_func = activations[output]
-        self.a_derivative = activation_derivatives[output]
+        self.o_derivative = activation_derivatives[output]
 
     def loss_function(self, loss='log_loss'):
         self.loss_func = losses[loss]
+        self.error = loss
 
     def train(self, iterations=100000, cost_interval=100):
         for i in range (iterations):
             self.feed = forward_propagate(self.X, self.params, self.layers, self.activation_func, self.output_func)
             self.cost = calculate_cost(self.Y, self.feed, self.layers, self.loss_func)
-            self.grads = backwards_propagate(self.X, self.Y, self.feed, self.layers, self.a_derivative)
-            self.params = update_params(self.params, self.grads,self.layers)
+            self.grads = backwards_propagate(self.X, self.Y, self.feed, self.layers, self.a_derivative, self.error)
+            self.params = update_params(self.params, self.grads, self.layers)
             if (i % cost_interval == 0) :
                 print(self.cost)
 
@@ -80,6 +81,7 @@ def relu(Z):
 
 # activation function for multiclass outputs | returns probability measure
 def softmax(Z):
+    max = np.apply_along_axis(np.max, 0, Z)
     ex = np.exp(Z)
     return ex/np.sum(ex, axis=0)
 
@@ -104,9 +106,12 @@ def tanh_prime(Z):
 
 # returns derivative of relu function
 def relu_prime(Z):
+    Z[np.isnan(Z)] = 0
+    Z[np.isinf(Z)] = 1
     Z[Z<=0] = 0
     Z[Z>0] = 1
     return Z
+
 # returns derivative of softmax_func | NEEDS OVERHAULS
 def softmax_prime(Z):
     return Z
@@ -129,16 +134,25 @@ Y denotes the labels(correct) predictions
 # returns the cost of the neural network using L2 Loss
 def squared_error(Y_h, Y):
     m = Y.shape[1]
-    np.log(Y_h)
     square_comp = np.square(Y_h, Y)/2
     sum_layers = np.sum(square_comp, axis=0)
     mean = np.mean(sum_layers)
     return np.squeeze(mean)
 
 # returns the cost of the neural network using Log Loss
+# def log_loss(Y_h, Y):
+#     m = Y.shape[1]
+#     log_comp = np.multiply(Y, np.log(Y_h)) + np.multiply((1-Y), np.log((1-Y_h)))
+#     sum_layers = - np.sum(log_comp, axis=0)
+#     mean = np.mean(sum_layers)
+#     return np.squeeze(mean)
+
 def log_loss(Y_h, Y):
+    # print(Y_h, Y)
     m = Y.shape[1]
-    log_comp = np.multiply(Y, np.log(Y_h)) + np.multiply((1-Y), np.log((1-Y_h)))
+    Y_h[Y_h<=0] = 10**-8
+    # log_comp = log_comparison(Y_h, Y) + log_comparison(1-Y_h, 1-Y)
+    log_comp =  np.multiply(Y, np.log(Y_h)) + np.multiply((1-Y), np.log((1-Y_h)))
     sum_layers = - np.sum(log_comp, axis=0)
     mean = np.mean(sum_layers)
     return np.squeeze(mean)
@@ -164,7 +178,6 @@ losses = {
 def output_error(Y_h, Y, loss_function='log_loss'):
     loss_string = error_command[loss_function]
     command_string = loss_string %('Y_h', 'Y')
-    # print(command_string)
     result = eval(command_string)
     return result
 
@@ -186,7 +199,7 @@ def define_parameters(model):
         pos = str(layer+1)
         # define dictionary keys for each weight/bias matrix accoridng to layer
         key_w, key_b = ('W' + pos ), ('b' + pos)
-        n_h = model[layer+1] # depth of the layer
+        n_h = model[int(pos)] # depth of the layer
         weights = np.random.randn(n_h, n_temp)
         biases = np.random.randn(n_h, 1)
         paramaters[key_w] = weights
@@ -204,7 +217,7 @@ def one_hot_encoder(Y, n):
     return result
 
 # performs forward propagtion
-def forward_propagate(X, parameters, layers, a_function=relu, o_function=sigmoid):
+def forward_propagate(X, parameters, layers, a_function=relu, o_function=softmax):
     activate_h = a_function # activation function for hidden layers
     activate_o = o_function # activation function for output layer
     A = X # current layer matrix(activation)
@@ -230,19 +243,19 @@ def forward_propagate(X, parameters, layers, a_function=relu, o_function=sigmoid
     return A, neuron_cache # final output and neuron_cache returned
 
 # compute the cost of the neural network using selected loss function
-def calculate_cost(Y, data, layers, loss_function=cross_entropy):
+def calculate_cost(Y, data, layers, loss_function=log_loss):
     Y_h, cache = data
     return loss_function(Y_h, Y)
 
 # performs backwards propagtion
-def backwards_propagate(X, Y, cache, layers, derivative, l_function='log_loss'):
+def backwards_propagate(X, Y, cache, layers, derivative, loss_function='cross_entropy'):
     Y_h, cache = cache
     a_derivative = derivative
     gradient_updates = dict()
     for layer in range(layers, 0, -1):
         pos = layer
         if (layer == layers):
-            dZ =  output_error(Y_h, Y, l_function)
+            dZ =  output_error(Y_h, Y, loss_function)
             E = dZ # donetes error
             gradient = backwards_propagate_params(X, Y, cache, pos, dZ)
         else:
@@ -257,7 +270,6 @@ def backwards_propagate(X, Y, cache, layers, derivative, l_function='log_loss'):
 
 # performs weights and biases gradient calculations
 def backwards_propagate_params(X, Y, cache, pos, dZ):
-    update = []
     m = X.shape[1]
     key_db, key_dW = 'db' + str(pos), 'dW' + str(pos)
     key_A = 'A' + str(pos-1)
@@ -269,6 +281,7 @@ def backwards_propagate_params(X, Y, cache, pos, dZ):
 
 # updates neural network paramaters accoridng to gradients and learning rate
 def update_params(params, gradients, layers, learning_rate=0.0005):
+    # note: dP is used to represent the partial derivates of biases/weights
     alpha = learning_rate
     params_keys = set(params.keys())            # get keys from paramater dictionary
     for (key, dP) in gradients.items():

scrap.py
@@ -12,16 +12,16 @@ import matplotlib.pyplot as plt
 from drawnow import *
 from tensorflow.examples.tutorials.mnist import input_data
 
-
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-
-X_train = np.vstack([img.reshape(-1,) for img in mnist.train.images])
-y_train = mnist.train.labels
-print(y_train.shape)
-
-X_train = X_train.T
-y_train = y_train.T
-print(X_train.shape, y_train.shape)
+np.random.seed(23)
+# mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+#
+# X_train = np.vstack([img.reshape(-1,) for img in mnist.train.images])
+# y_train = mnist.train.labels
+# print(y_train.shape)
+#
+# X_train = X_train.T
+# y_train = y_train.T
+# print(X_train.shape, y_train.shape)
 
 # with open('data.txt', 'w') as file:
 #     file.write('X:' + str(X_train))
@@ -50,16 +50,24 @@ X = X.T
 # test = np.array(test, dtype='float')
 
 # Number of training examples
-m = X_train.shape[1]
+# m = X_train.shape[1]
 # Y = np.reshape(data.target, (1,m))
 # number of input/output neurons
-n_x = X_train.shape[0]
+# n_x = X_train.shape[0]
 # n_y = Y.shape[0]
 n_y = 10
 
+
+data_1 = load_iris()
+X_train_1 = data_1.data
+X_train_1 = X_train_1.T
+m = X_train_1.shape[1]
+Y_train_1 = np.reshape(data_1.target.T, (1,m))
+n_x = X_train_1.shape[0]
+n_y = 3                                 # set value for number of output neurons
+model_1 = [n_x, 4, 6, 4, n_y]
 # nueral network model
 model = [n_x, 6, 8, 5, n_y]
-
 
 # number of layers
 layers = len(model) - 1
@@ -83,7 +91,8 @@ def relu(Z):
 
 # activation function for multiclass outputs | returns probability measure
 def softmax(Z):
-    ex = np.exp(Z - np.max(Z))
+    max = np.apply_along_axis(np.max, 0, Z)
+    ex = np.exp(Z)
     return ex/np.sum(ex, axis=0)
 
 activations = {
@@ -107,6 +116,8 @@ def tanh_prime(Z):
 
 # returns derivative of relu function
 def relu_prime(Z):
+    Z[np.isnan(Z)] = 0
+    Z[np.isinf(Z)] = 1
     Z[Z<=0] = 0
     Z[Z>0] = 1
     return Z
@@ -130,7 +141,6 @@ Y denotes the labels(correct) predictions
 # returns the cost of the neural network using L2 Loss
 def squared_error(Y_h, Y):
     m = Y.shape[1]
-    np.log(Y_h)
     square_comp = np.square(Y_h, Y)/2
     sum_layers = np.sum(square_comp, axis=0)
     mean = np.mean(sum_layers)
@@ -138,11 +148,31 @@ def squared_error(Y_h, Y):
 
 # returns the cost of the neural network using Log Loss
 def log_loss(Y_h, Y):
+    # print(Y_h, Y)
     m = Y.shape[1]
-    log_comp = np.multiply(Y, np.log(Y_h)) + np.multiply((1-Y), np.log((1-Y_h)))
+    Y_h[Y_h<=0] = 10**-8
+    # log_comp = log_comparison(Y_h, Y) + log_comparison(1-Y_h, 1-Y)
+    log_comp =  np.multiply(Y, np.log(Y_h)) + np.multiply((1-Y), np.log((1-Y_h)))
     sum_layers = - np.sum(log_comp, axis=0)
     mean = np.mean(sum_layers)
     return np.squeeze(mean)
+
+def log_comparison(Y_h, Y):
+    rows, cols = Y.shape[0], Y.shape[1]
+    result = np.zeros((rows, cols))
+    for row in range(rows):
+        for col in range(cols):
+            y_h, y = Y_h[row, col], Y[row, col]
+            if y == 0:
+                continue
+            else:
+                try:
+                    comp = np.multiply(y, np.log(y_h))
+                except:
+                    # print(y_h)
+                    comp = np.multiply(y, np.log(y_h+0.00001))
+                result[row, col] = comp
+    return result
 
 # returns the cost of the neural network using Cross Entropy
 def cross_entropy(Y_h, Y):
@@ -197,9 +227,9 @@ def one_hot_encoder(Y, n):
         val = Y[0,i]
         result[val,i] = 1
     return result
-
+relu
 # performs forward propagtion
-def forward_propagate(X, parameters, layers, a_function='tanh', o_function='softmax'):
+def forward_propagate(X, parameters, layers, a_function='relu', o_function='softmax'):
     activate_h = activations[a_function] # activation function for hidden layers
     activate_o = activations[o_function] # activation function for output layer
     A = X # current layer matrix(activation)
@@ -225,7 +255,7 @@ def forward_propagate(X, parameters, layers, a_function='tanh', o_function='soft
     assert(A.shape[0] == n_y)
     return A, neuron_cache # final output and neuron_cache returned
 
-def calculate_cost(Y, data, layers, loss_function='cross_entropy'):
+def calculate_cost(Y, data, layers, loss_function='log_loss'):
     Y_h, cache = data
     cost_function = losses[loss_function]
     return cost_function(Y_h, Y)
@@ -254,7 +284,6 @@ def backwards_propagate(X, Y, cache, layers,  l_function='log_loss'):
 
 # performs forward propagtion
 def backwards_propagate_helper(X, Y, cache, pos, dZ):
-    update = []
     m = X.shape[1]
     key_db, key_dW = 'db' + str(pos), 'dW' + str(pos)
     key_A = 'A' + str(pos-1)
@@ -277,8 +306,8 @@ def update_params(params, gradients, layers, learning_rate=0.0005):
 # X = np.random.randn(n_x, m)
 # Y = np.random.randn(model[-1],m)
 
-params = define_parameters(model)
-# y_train = one_hot_encoder(y_train, n_y)
+params = define_parameters(model_1)
+Y_train_1 = one_hot_encoder(Y_train_1, n_y)
 cost_list = []
              #Set y min and max values based on known voltage quantity
 plt.grid(True)
@@ -288,13 +317,13 @@ plt.axis([0, 50000, 0, 10])
 plt.ion()
 
 
-print(X_train)
-for i in range (10000):
+print(X_train_1)
+for i in range (50000):
     cost_list = []
     index = []
-    data = forward_propagate(X_train, params, layers)
-    cost = calculate_cost(y_train, data, layers)
-    grads = backwards_propagate(X_train, y_train, data, layers)
+    data = forward_propagate(X_train_1, params, layers)
+    cost = calculate_cost(Y_train_1, data, layers)
+    grads = backwards_propagate(X_train_1, Y_train_1, data, layers)
     params = update_params(params, grads, layers)
     if (i % 1000 == 0) :
         print(cost)